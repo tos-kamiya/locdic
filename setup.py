d241179ee4aa8ae00e9c37112715d1d8833e6707
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2012-01-04 17:46:06 +0900

    Fixed broken setup.py.

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from distutils.core import setup
 def glob_wo_dirs(w):
     return [f for f in glob.glob(w) if not os.path.isdir(f)]
 
+def target_dir(relp):
+    return os.path.join("/usr/local/lib/locdic", relp)
+
 setup(
     name='locdic',
     version='0.1.0',
@@ -14,25 +17,22 @@ setup(
     author="Toshihiro Kamiya",
     author_email="kamiya@mbj.nifty.com",
     url="http://www.remics.org/",
-    py_modules=[
-        '_config', 'ldfind', 'ldweb',
-    ],
-    packages=[
-        'engine'
-    ],
     data_files=[
-        ('data', [ 'data/readme', 'data/import_gene.py' ]),
-        ('doc', [ 'doc/LICENSE', 'doc/README' ]),
-        ('static', glob_wo_dirs('static/*')),
-        ('static/images', glob_wo_dirs('static/images/*')),
-        ('view', glob_wo_dirs('view/*')),
+        (target_dir(''), glob_wo_dirs('*.py')),
+        (target_dir('engine'), glob_wo_dirs('engine/*.py')),
+        (target_dir('test'), glob_wo_dirs('test/*.py')),
+        (target_dir('data'), [ 'data/readme', 'data/import_gene.py' ]),
+        (target_dir(''), [ 'LICENSE', 'README' ]),
+        (target_dir('static'), glob_wo_dirs('static/*')),
+        (target_dir('static/images'), glob_wo_dirs('static/images/*')),
+        (target_dir('view'), glob_wo_dirs('view/*')),
     ],
-    install_requires=[
-        "bottle >= 0.10",
+    requires=[
+        "bottle (>=0.10)",
     ],
       
-    liecense="doc/LICENSE",
-    long_description="doc/README",
+    license="LICENSE",
+    long_description="README",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
